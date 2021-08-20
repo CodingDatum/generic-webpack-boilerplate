039f8eecb8b5ba9463a3f880b2ec74f46da06efd
Matthew Cain <codingdatum@gmail.com>
2021-08-20 11:51:27 -0400

    this is right before we attempt the newer version of webpack-dev-server that crashed

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,5 @@
+import "../styles/styles.css";
+
+if(module.hot){
+    module.hot.accept(); //telling the browser to accept these hot updates
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// CONSIDER MAKING THIS WHOLE PROJECT A BOILER PLATE.
+// START WITH THE BARE BONE WEBPACK CONFIG FILES AND HAVE THE INSTALABLE FOR NPM UPDATES.
+
+const path = require("path");
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: "./app/assets/scripts/App.js",
+    output: {
+        filename: "bundled.js",
+        path: path.resolve(__dirname, "app")
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000
+    },
+    mode: "development",
+    // watch: true, (we no longer need this because of our devServer property)
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
+            }
+        ]
+    }
+}
